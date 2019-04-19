944640369db3e409dd91f51a48a52190015196cb
Peer Rheinboldt <peer.rheinboldt@gmail.com>
2019-04-19 18:23:20 +0200

    Changed dunst install

--- wal/install.sh
+++ wal/install.sh
@@ -8,3 +8,6 @@ ln -s $HOME/.dotfiles/wal/templates $HOME/.config/wal/
 
 cp -r $HOME/.dotfiles/wal/vs-wal $HOME/.vscode-oss/extensions/
 ln -s $HOME/.cache/wal/vscode.json $HOME/.vscode-oss/extensions/vs-wal/themes/vs-wal-color-theme.json
+
+wal --theme ~/.dotfiles/wal/themes/dracula.json
+ln -sf $HOME/.cache/wal/dunstrc $HOME/.config/dunst/dunstrc
