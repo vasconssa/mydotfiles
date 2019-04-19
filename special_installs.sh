08b7c6c4ba242fa2c008f906eb02f5450e493717
Peer Rheinboldt <peer.rheinboldt@gmail.com>
2019-04-19 22:16:43 +0200

    suckless terminal

--- special_installs.sh
+++ special_installs.sh
@@ -7,3 +7,11 @@ sudo systemctl enable bluetooth.service
 
 # enable networkmanager
 sudo systemctl enable NetworkManager.service
+
+# st
+mkdir ~/.builds
+git clone https://github.com/LukeSmithxyz/st.git ~/.builds/st
+cd ~/.builds/st
+make
+sudo make install
+cd ~/.dotfiles
