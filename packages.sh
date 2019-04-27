097886259aec6765c8c24a18942aa4a8c5986422
Peerlator <peer.rheinboldt@gmail.com>
2019-04-27 11:24:24 +0200

    Fixed Packages.sh

--- packages.sh
+++ packages.sh
@@ -1,40 +1,4 @@
 #!/bin/bash
 
-export PKGS=(
-  bat
-  bluez
-  bluez-utils
-  curl
-  diff-so-fancy
-  dunst
-  feh
-  htop
-  libreoffice-still
-  lxappearance
-  neofetch
-  nerd-fonts-fira-code
-  networkmanager
-  playerctl
-  pulseaudio
-  pulseaudio-alsa
-  pulseaudio-bluetooth
-  pulsemixer
-  ranger
-  tldr
-  tree
-  ttf-liberation
-  unzip
-  wget
-  xclip
-  zathura-pdf-mupdf
-  betterlockscreen
-  franz
-  chromium 
-  neovim
-  polybar
-  screenkey
-  compton
-  spotify
-  watchexec
-)
+export PKGS=(bat bluez bluez-utils curl diff-so-fancy dunst feh htop libreoffice-still lxappearance neofetch nerd-fonts-fira-code networkmanager playerctl pulseaudio pulseaudio-alsa pulseaudio-bluetooth pulsemixer ranger tldr tree ttf-liberation unzip wget xclip zathura-pdf-mupdf betterlockscreen franz chromium  neovim polybar screenkey compton spotify watchexec)
 
