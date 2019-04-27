14021ef64d8abdde4bfe34da036b22cca0c675a4
Peerlator <peer.rheinboldt@gmail.com>
2019-04-27 11:28:30 +0200

    Fixed distro.sh

--- distro.sh
+++ distro.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 export PKGMN=yay # The packagemanager to use
-export PKGOPT=(--needed --no-confirm) # The options passed when installing packages
+export PKGOPT=(--needed --noconfirm) # The options passed when installing packages
 export PKGU=(-Syu) # Updating options
 export PKGI=(-S) # Installation options
